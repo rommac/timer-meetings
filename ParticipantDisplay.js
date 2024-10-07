9089ce5d21a0211409c2dc344ac0eb7ce3b84c27
rommac <rommac@users.noreply.github.com>
2024-10-06 23:22:57 -0500

    Create ParticipantDisplay.js

---
+++ ParticipantDisplay.js
@@ -0,0 +1,10 @@
+function ParticipantDisplay({ participant }) {
+  return (
+    <div>
+      <h2>Participante Actual:</h2>
+      <p>{participant}</p>
+    </div>
+  );
+}
+
+export default ParticipantDisplay;
